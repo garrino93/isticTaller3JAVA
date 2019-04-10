a8a92559a6d7200ecd9378ebd53df13e67bb663f
garrion93 <49006266+garrion93@users.noreply.github.com>
2019-04-10 20:22:11 -0300

    Add files via upload

---
+++ Trabajo Práctico/tpEntradaSalida/src/tpentradasalida/FuncionesEstaticas.java
@@ -0,0 +1,52 @@
+package tpentradasalida;
+
+
+public class FuncionesEstaticas 
+{
+   
+    public static void Saludar()
+    {
+        System.out.println("Bienvenido a JAVA");
+    }
+
+    public static void SaludarConNombre(String Nombre)
+    {   
+        System.out.println("bienvenido a java " + Nombre );
+    }
+   
+    public static void SaludarConNombreYApellido(String Nombre, String Apellido)
+    {
+        System.out.println("su nombre es " + Nombre + " y " + Apellido + " es su apellido");
+    }
+    
+    public static void SaludarConNombreYEdad(String Nombre, int Edad)
+    {
+        System.out.println("Usted se llama " + Nombre + " y tiene " + Edad + " años de edad");
+    }
+    
+    public static void SumarSinRetorno(int numero1, int numero2)
+    {
+        int result;
+        
+        result = numero1 + numero2;
+        
+        System.out.println(result);
+    }
+    public static int SumarConRetorno(int numero1, int numero2)
+    {
+        int suma;
+        
+        suma = numero1 + numero2;
+        
+        return suma;
+    }
+    public static double Promedio(int numero1, int numero2, int numero3)
+    {
+        double Promedio;
+        
+        Promedio = (numero1 + numero2 + numero3) / 3;
+        
+        return Promedio;
+    }
+}
+    
---
+++ Trabajo Práctico/tpEntradaSalida/src/tpentradasalida/TpEntradaSalida.java
@@ -0,0 +1,40 @@
+package tpentradasalida;
+
+
+public class TpEntradaSalida
+{
+
+    public static void main(String[] args)
+    {
+        FuncionesEstaticas.Saludar();
+           
+        System.out.println("------------------------------------");
+           
+        FuncionesEstaticas.SaludarConNombre("Pepito");
+        
+        System.out.println("------------------------------------");
+        
+        FuncionesEstaticas.SaludarConNombreYApellido("Pepito", "Valdivia");
+        
+        System.out.println("------------------------------------");
+        
+        FuncionesEstaticas.SaludarConNombreYEdad("Pepito", 35);
+        
+        System.out.println("------------------------------------");
+        
+        FuncionesEstaticas.SumarSinRetorno(5, 10);
+        
+        System.out.println("------------------------------------");
+        
+        FuncionesEstaticas.SumarConRetorno(4, 6);
+        
+        System.out.println("------------------------------------");
+        
+        FuncionesEstaticas.Promedio(4, 6, 2);
+        
+        System.out.println("------------------------------------");
+        
+        
+        
+    }
+}
